3556555a9fd0f095eba7246f743cf65bd59283a6
Malac <54860577+Malac-253@users.noreply.github.com>
2026-04-02 11:02:12 -0400

    Add files via upload

---
+++ config/routes.rb
@@ -0,0 +1,31 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Render dynamic PWA files from app/views/pwa/*
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+
+  root "blog_posts#index"
+
+  resources :blog_posts # does this automatically generate the routes for the blog_posts controller?
+  # get "/blog_posts", to: "blog_posts#index", as: :all_blog_posts
+  # get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
+  # get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  # patch "/blog_posts/:id", to: "blog_posts#update", as: :update_blog_post
+  # delete "/blog_posts/:id", to: "blog_posts#destroy", as: :destroy_blog_post
+  # get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
+  # post "/blog_posts", to: "blog_posts#create", as: :blog_posts
+
+  
+  # blog_post_path(1) => "/blog_posts/1"
+  # blog_post_url(1) => "https://www.example.com/blog_posts/1"
+
+end
